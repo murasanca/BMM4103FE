5bd0397ce7e9fc1c766125bdc8f7ebd858d346a9
Murat Sancak <murasanca@protonmail.com>
2023-01-09 00:53:38 +0300

    2023.1.9

--- app/src/main/java/com/murasanca/BMM4103FE/HomeActivity.java
+++ app/src/main/java/com/murasanca/BMM4103FE/HomeActivity.java
@@ -2,8 +2,15 @@ package com.murasanca.BMM4103FE;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity
 {
@@ -20,5 +27,81 @@ public class HomeActivity extends AppCompatActivity
 				);
 		
 		setContentView(R.layout.activity_home);
+		
+		FrameLayout
+				loginFrameLayout=findViewById(R.id.loginFrameLayout),
+				signUpFrameLayout=findViewById(R.id.signUpFrameLayout);
+		loginFrameLayout.setVisibility(getIntent().getIntExtra("loginFrameLayoutVisibility",View.VISIBLE));
+		signUpFrameLayout.setVisibility(getIntent().getIntExtra("signUpFrameLayoutVisibility",View.GONE));
+		
+		Intent HomeActivity2NavigationDrawerActivity=new Intent(HomeActivity.this,NavigationDrawerActivity.class);
+		FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
+		
+		EditText
+				emailLoginEditText=findViewById(R.id.emailLoginEditText),
+				emailSignUpEditText=findViewById(R.id.emailSignUpEditText),
+				passwordLoginEditText=findViewById(R.id.passwordLoginEditText),
+				passwordSignUpEditText=findViewById(R.id.passwordSignUpEditText);
+		
+		// Login Button at Login Frame Layout
+		findViewById(R.id.loginLoginButton).setOnClickListener
+				(
+						v->
+						{
+							if(!(emailLoginEditText.getText()==null&&null==passwordLoginEditText.getText()))
+								firebaseAuth.signInWithEmailAndPassword(emailLoginEditText.getText().toString(),passwordLoginEditText.getText().toString()).addOnCompleteListener
+										(
+												this,task ->
+												{
+													if(task.isSuccessful())
+													{
+														startActivity(HomeActivity2NavigationDrawerActivity);
+														overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+													}
+													else
+														Toast.makeText(getApplicationContext(),"Task is not successful.",Toast.LENGTH_SHORT).show();
+												}
+										);
+						}
+				);
+		
+		// Login Button at Sign Up Frame Layout
+		findViewById(R.id.loginSignUpButton).setOnClickListener
+				(
+						v->
+						{
+							loginFrameLayout.setVisibility(View.VISIBLE);
+							signUpFrameLayout.setVisibility(View.GONE);
+						}
+				);
+		
+		// Sign Up Button at Login Frame Layout
+		findViewById(R.id.signUpLoginButton).setOnClickListener
+				(
+						v->
+						{
+							loginFrameLayout.setVisibility(View.GONE);
+							signUpFrameLayout.setVisibility(View.VISIBLE);
+						}
+				);
+		
+		// Sign Up Button at Sign Up Frame Layout
+		findViewById(R.id.signUpSignUpButton).setOnClickListener
+				(
+						v->firebaseAuth.createUserWithEmailAndPassword(emailSignUpEditText.getText().toString(),passwordSignUpEditText.getText().toString()).addOnCompleteListener
+								(
+										this,task ->
+										{
+											if(task.isSuccessful())
+											{
+												startActivity(HomeActivity2NavigationDrawerActivity);
+												overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+											}
+											else
+												Toast.makeText(getApplicationContext(),"Task is not successful.",Toast.LENGTH_SHORT).show();
+										}
+										
+								)
+				);
 	}
 }
--- app/src/main/java/com/murasanca/BMM4103FE/MainActivity.java
+++ app/src/main/java/com/murasanca/BMM4103FE/MainActivity.java
@@ -2,8 +2,14 @@ package com.murasanca.BMM4103FE;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -20,5 +26,62 @@ public class MainActivity extends AppCompatActivity
 				);
 		
 		setContentView(R.layout.activity_main);
+		
+		Intent
+				MainActivity2HomeActivity=new Intent(MainActivity.this,HomeActivity.class),
+				MainActivity2NavigationDrawerActivity=new Intent(MainActivity.this,NavigationDrawerActivity.class);
+		FirebaseAuth firebaseAuth=FirebaseAuth.getInstance();
+		FrameLayout
+				mainFrameLayout=findViewById(R.id.mainFrameLayout),
+				splashScreenFrameLayout=findViewById(R.id.splashScreenFrameLayout);
+		
+		firebaseAuth.signOut();
+		
+		if(firebaseAuth.getCurrentUser()==null)
+		{
+			new Handler().postDelayed
+					(
+							()->
+							{
+								splashScreenFrameLayout.setVisibility(View.GONE);
+								mainFrameLayout.setVisibility(View.VISIBLE);
+							},
+							2048
+					);
+		}
+		else //if(firebaseAuth.getCurrentUser()!=null)
+		{
+			new Handler().postDelayed
+					(
+							()->
+							{
+								startActivity(MainActivity2NavigationDrawerActivity);
+								overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+							},
+							2048
+					);
+		}
+		
+		findViewById(R.id.loginMainButton).setOnClickListener
+				(
+						v->
+						{
+							MainActivity2HomeActivity.putExtra("loginFrameLayoutVisibility",View.VISIBLE);
+							MainActivity2HomeActivity.putExtra("signUpFrameLayoutVisibility",View.GONE);
+							startActivity(MainActivity2HomeActivity);
+							overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+						}
+				);
+		
+		findViewById(R.id.signUpMainButton).setOnClickListener
+				(
+						v->
+						{
+							MainActivity2HomeActivity.putExtra("loginFrameLayoutVisibility",View.GONE);
+							MainActivity2HomeActivity.putExtra("signUpFrameLayoutVisibility",View.VISIBLE);
+							startActivity(MainActivity2HomeActivity);
+							overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);
+						}
+				);
 	}
 }
